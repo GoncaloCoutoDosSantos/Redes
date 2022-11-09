583fb3b505bc697f74eb133f884effb65b186165
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-11-09 19:46:49 +0000

    fix packet

--- Packet.py
+++ Packet.py
@@ -13,15 +13,20 @@ class Packet:
 		ret = ret + str(arr[0])
 		for i in range(1,4):
 			ret = ret + "." + str(arr[i])
-		return ret.encode("utf-8")
+		return ret
 
 	def encode_LSA(addr,vizinhos_dic):
 		vizinhos = vizinhos_dic.keys()
-		packet = bytearray(6+ (len(vizinhos) * 5))
+		packet = bytearray(1 + 1 + len(addr) + 1 + (len(vizinhos) * 5))
 		packet[0] = 0 # Set mode LSA
-		Packet.ip_to_byte(addr,packet,1) #Put IP host
-		packet[5] = len(vizinhos)# Number of vizinhos
-		ind = 6
+		packet[1] = len(addr)
+		ind = 2
+		for i in addr:
+			packet[ind] = ord(i)
+			ind = ind + 1
+
+		packet[ind] = len(vizinhos)# Number of vizinhos
+		ind = ind + 1
 		for i in vizinhos:
 			Packet.ip_to_byte(i,packet,ind) # IP vizinhos
 			ind = ind + 4
@@ -32,10 +37,11 @@ class Packet:
 	def decode_LSA(packet):
 		#Packet LSA
 		print("Receive LSA packet")
-		host = Packet.byte_to_ip(packet[1:]) #Ip do router original
+		host = str(packet[2:2 + packet[1]])
+		print(host)
 		vizinhos = []
-		ind = 6
-		for i in range(packet[5]): # Percorre lista dos ip dos vizinhos
+		ind = 2 + packet[1] +1
+		for i in range(packet[ind-1]): # Percorre lista dos ip dos vizinhos
 			vizinhos.append((Packet.byte_to_ip(packet[ind:]),packet[ind + 4]))
 			ind = ind + 5
 		return (host,vizinhos)
@@ -48,8 +54,8 @@ if __name__ == '__main__':
 	vizinhos["10.0.0.21"] = "ola"
 	vizinhos["10.0.1.20"] = "ola"
 
-	packet = Packet.encode_lsa("10.0.0.20",vizinhos) 
+	packet = Packet.encode_LSA("n2",vizinhos) 
 
 	print(packet)
-	print(Packet.decode(packet))
+	print(Packet.decode_LSA(packet))
 
