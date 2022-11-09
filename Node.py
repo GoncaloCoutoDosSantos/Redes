16cb67d665f3d297f43f5bc90ab38ae9b5950a19
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2022-11-09 19:04:52 +0000

    topologia

--- Node.py
+++ Node.py
@@ -1,14 +1,17 @@
 import socket, threading, sys
+from Packet import Packet
 
 class Node:
-	def __init__(self,vizinhos,port = 12451):
+	top = {}
+	def __init__(self,vizinhos,port = 12455):
 		self.flag = True
-		self.host = vizinhos[0] #cant be local host 
+		self.host =  socket.gethostname() #cant be local host 
 		print("IP:",self.host)
 		self.port = port
-		self.vizinhos_all = vizinhos[1:]
+		self.vizinhos_all = vizinhos
 		print("Todos vizinhos:",self.vizinhos_all)
 		
+		self.top[self.host] = []
 		self.vizinhos = {}
 
 		#tenta ligar se aos vizinhos(ve os que estao ativos)
@@ -17,30 +20,45 @@ class Node:
 			try:
 				s.connect((i,self.port))
 				self.vizinhos[i] = s
+				self.top[self.host].append(i)
 				print("Vizinho Ativo:",i)
 				threading.Thread(target=self.recv,args=(s,i)).start()
 			except:
 				s.close()
 				print("node {} not active".format(i))
 
-		print("Vizinhos Ativos:",self.vizinhos.keys())
+		self.send_LSA()
+		self.status()
 
 		self.s = socket.socket()
-		self.s.bind((self.host,port))
+		self.s.bind(("",port))
 
 		#listen for new connections
 		self.s.listen(5)
 		while self.flag:
 			c, addr = self.s.accept()
 			print('Got connection from', addr)
-			print("{} in {}:{}".format(addr[0],self.vizinhos_all,addr in self.vizinhos_all))
-			if(addr[0] in self.vizinhos_all):
-				self.vizinhos[addr] = c
+			print("{} in {}:{}".format(addr[0],self.vizinhos_all,addr[0] in self.vizinhos_all))
+			if(True):#addr[0] in self.vizinhos_all):
+				self.vizinhos[addr[0]] = c
 				threading.Thread(target=self.recv,args=(c,addr)).start()
-				print("Vizinhos Ativos:",self.vizinhos.keys())
+				self.top[self.host].append((addr[0],1))
+				self.send_LSA()
+				self.status()
 			else:
 				c.close()
 
+	def send_LSA(self):
+		print("host"+str(self.host))
+		packet = Packet.encode_LSA(self.host,self.vizinhos)
+		for i in self.vizinhos:
+				print(type(self.vizinhos[i]))
+				self.vizinhos[i].send(packet)
+
+	def send_flood(self,packet,addr = ""):
+		for i in self.vizinhos:
+			if i != addr:
+				self.vizinhos[i].send(packet)
 
 	#call to create treat to send
 	def send_normal(self,msg,host,port):
@@ -55,10 +73,27 @@ class Node:
 	def recv(self,s,addr):
 		while self.flag:
 			data  = s.recv(1024)
-			print("receive data from {}:{}".format(addr,data))
+			if(data[0] == 0):
+				print("receive LSA from {}:".format(addr))
+				ori,vizinhos = Packet.decode_LSA(data)
+				print("Origem: {} | Vizinhos: {}".format(ori,vizinhos))
+				
+				if(not(ori in self.top) or self.top[ori] != vizinhos):
+					self.top[ori] = vizinhos
+					self.send_flood(data)
+					self.status()
+			else:
+				print("Receive from {} data:{}".format(addr,data))
+
+	def status(self):
+		print("Vizinhos Ativos:",self.vizinhos.keys())
+		print("Topologia:")
+		for i in self.top:
+			print("Node {}:{}".format(i,self.top[i]))
 
 	def close(self):
-		self.s.close();
+		pass
+		#self.s.close();
 
 
 t1 = Node(sys.argv[1:])
--- Packet.py
+++ Packet.py
@@ -8,24 +8,48 @@ class Packet:
 		for i in range(4):
 			arr[ind + i] = int(ip[i])
 
-	def encode_lsa(addr,vizinhos_dic):
+	def byte_to_ip(arr):
+		ret = ""
+		ret = ret + str(arr[0])
+		for i in range(1,4):
+			ret = ret + "." + str(arr[i])
+		return ret.encode("utf-8")
+
+	def encode_LSA(addr,vizinhos_dic):
 		vizinhos = vizinhos_dic.keys()
 		packet = bytearray(6+ (len(vizinhos) * 5))
-		packet[0] = 0
-		Packet.ip_to_byte(addr,packet,1)
-		packet[5] = len(vizinhos)
+		packet[0] = 0 # Set mode LSA
+		Packet.ip_to_byte(addr,packet,1) #Put IP host
+		packet[5] = len(vizinhos)# Number of vizinhos
 		ind = 6
 		for i in vizinhos:
-			Packet.ip_to_byte(i,packet,ind)
+			Packet.ip_to_byte(i,packet,ind) # IP vizinhos
 			ind = ind + 4
-			packet[ind] = 1
+			packet[ind] = 1 #Set Custo
 			ind = ind + 1
 		return packet
 
+	def decode_LSA(packet):
+		#Packet LSA
+		print("Receive LSA packet")
+		host = Packet.byte_to_ip(packet[1:]) #Ip do router original
+		vizinhos = []
+		ind = 6
+		for i in range(packet[5]): # Percorre lista dos ip dos vizinhos
+			vizinhos.append((Packet.byte_to_ip(packet[ind:]),packet[ind + 4]))
+			ind = ind + 5
+		return (host,vizinhos)
 
-vizinhos = {}
-vizinhos["10.0.0.21"] = "ola"
-vizinhos["10.0.1.20"] = "ola"
 
-print(Packet.encode_lsa("10.0.0.20",vizinhos))
+
+
+if __name__ == '__main__':
+	vizinhos = {}
+	vizinhos["10.0.0.21"] = "ola"
+	vizinhos["10.0.1.20"] = "ola"
+
+	packet = Packet.encode_lsa("10.0.0.20",vizinhos) 
+
+	print(packet)
+	print(Packet.decode(packet))
 
