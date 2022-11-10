d2aa0f03db03d3260f03ec3968777afed543200d
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2022-11-10 12:08:52 +0000

    CC packet

--- Packet.py
+++ Packet.py
@@ -1,7 +1,7 @@
 
 class Packet:
 	def __init__():
-	 	pass
+		pass
 
 	def ip_to_byte(ip,arr,ind = 0):
 		ip = ip.split(".")
@@ -46,7 +46,28 @@ class Packet:
 			ind = ind + 5
 		return (host,vizinhos)
 
+	def encode_CC(addr,tempoI,tempos):
+		array = bytearray(5)
+		array[0] = int(1)
+		Packet.ip_to_byte(addr,array,1) #Put IP host
+		tempoByteA = tempoI.to_bytes(10,'big')
+		tamanho = len(tempos).to_bytes(4,'big')
+		temposByteA = []
+		for i in tempos:
+			temposByteA.append(i.to_bytes(10,'big'))
+		return array+tempoByteA+tamanho+tempoByteA
 
+	def decode_CC(packet):
+		print(packet)
+		print("Receive CC packet")
+		host = Packet.byte_to_ip(packet[1:5]) #Ip do router original
+		tempoI = int.from_bytes(packet[5:15],'big')
+		tempos = []
+		ind = 19
+		for i in range(int.from_bytes(packet[15:19],'big')): 
+			tempos.append(int.from_bytes(packet[ind:ind+10],'big'))
+			ind += 10
+		return (host,tempoI,tempos)
 
 
 if __name__ == '__main__':
