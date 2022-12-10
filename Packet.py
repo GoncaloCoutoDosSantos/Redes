a189c83cee67bd26479bd352bbca84fb9b79fd84
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2022-12-10 01:33:55 +0000

    Sorry for reversing your commit Xico

--- Packet.py
+++ Packet.py
@@ -103,52 +103,6 @@ class Packet:
 	def decode_NACK(packet): #Node with no acess to server addr
 		return packet[0]
 
-	def encodeRTP(self, version, padding, extension, cc, seqnum, marker, pt, ssrc, payload):
-		"""Encode the RTP packet with header fields and payload."""
-		timestamp = int(time())
-		header = bytearray(HEADER_SIZE)
-		header[0] = int(4)#version
-		header[1] = int(pt)
-		header[2] = int(extension)
-		header[3] = int(seqnum)
-		header[4] = int(cc)	
-		header[5] = int(marker)
-		header[6] = int(padding)
-		header[7] = int(timestamp)
-		header[8] = int(ssrc)
-		self.payload = payload
-		
-	def decodeRTP(self, byteStream):
-		"""Decode the RTP packet."""
-		self.header = bytearray(byteStream[:HEADER_SIZE])
-		self.payload = byteStream[HEADER_SIZE:]
-	
-	def version(self):
-		"""Return RTP version."""
-		return int(self.header[0] >> 6)
-
-	def payloadType(self):
-		"""Return payload type."""
-		pt = self.header[1] & 127
-		return int(pt)
-		
-	def seqNum(self):
-		"""Return sequence (frame) number."""
-		seqNum = self.header[2] << 8 | self.header[3]
-		return int(seqNum)
-	
-	def timestamp(self):
-		"""Return timestamp."""
-		timestamp = self.header[4] << 24 | self.header[5] << 16 | self.header[6] << 8 | self.header[7]
-		return int(timestamp)
-	
-	def getPayload(self):
-		"""Return payload."""
-		return self.payload
-		
-	def getRTPPacket(self):
-		"""Return RTP packet."""
-		return self.header + self.payload
 
 if __name__ == '__main__':
 	vizinhos = {}
